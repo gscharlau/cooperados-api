55029344095568675f02d23ab7931d7d3f3aa487
Guilherme Scharlau <gscharlau@gmail.com>
2019-08-06 11:29:47 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Build Status](https://travis-ci.org/gscharlau/cooperados-api.svg?branch=master)](https://travis-ci.org/gscharlau/cooperados-api)
 # cooperados-api
---
+++ src/main/java/br/com/ntconsult/cooperadosapi/CooperadosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ntconsult.cooperadosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperadosApiApplication.class, args);
+	}
+
+}
