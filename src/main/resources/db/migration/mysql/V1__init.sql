8cdf4864364da0be93d833e12ccecb11c017b05f
Guilherme Scharlau <gscharlau@gmail.com>
2019-08-07 01:04:54 -0300

    Alteração Database

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -1,14 +1,19 @@
 -- Criação de Tabela de Cooperados --
-CREATE TABLE cooperado ('id' BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT, 'cpf' VARCHAR(15) NOT NULL, 'nome' VARCHAR(150) NOT NULL, PRIMARY KEY ('id')) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE public.cooperado (
+	id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+	cpf varchar(15) NOT NULL,
+	nome varchar(100) NOT NULL
+);
+
 
 -- Carga Inicial --
-INSERT INTO cooperado (cpf, nome) VALUES ('07262076167', 'Cooperado 001');
-INSERT INTO cooperado (cpf, nome) VALUES ('51066194165', 'Cooperado 002');
-INSERT INTO cooperado (cpf, nome) VALUES ('95746638501', 'Cooperado 003');
-INSERT INTO cooperado (cpf, nome) VALUES ('23302747772', 'Cooperado 004');
-INSERT INTO cooperado (cpf, nome) VALUES ('24481361573', 'Cooperado 005');
-INSERT INTO cooperado (cpf, nome) VALUES ('99131576257', 'Cooperado 006');
-INSERT INTO cooperado (cpf, nome) VALUES ('11775765890', 'Cooperado 007');
-INSERT INTO cooperado (cpf, nome) VALUES ('35573415778', 'Cooperado 008');
-INSERT INTO cooperado (cpf, nome) VALUES ('51343483409', 'Cooperado 009');
-INSERT INTO cooperado (cpf, nome) VALUES ('41449059422', 'Cooperado 010');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('07262076167', 'Cooperado 001');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('51066194165', 'Cooperado 002');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('95746638501', 'Cooperado 003');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('23302747772', 'Cooperado 004');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('24481361573', 'Cooperado 005');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('99131576257', 'Cooperado 006');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('11775765890', 'Cooperado 007');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('35573415778', 'Cooperado 008');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('51343483409', 'Cooperado 009');
+INSERT INTO public.cooperado (cpf, nome) VALUES ('41449059422', 'Cooperado 010');
